d889931e8adede1608eadfe655994c86737b235d
Piotr Blaszyk <piopio555888@gmail.com>
2025-04-10 18:59:30 +0100

    dry

--- difftactile/tasks/object_repose.py
+++ difftactile/tasks/object_repose.py
@@ -516,22 +516,6 @@ class Contact:
         self.fem_sensor1.get_external_force(self.fem_sensor1.sub_steps - 2)
         self.mpm_object.get_external_force(self.mpm_object.sub_steps - 2)
         self.compute_contact_force(self.sub_steps - 2)
-
-    def render(self, gui1, gui2, gui3):
-        viz_scale = 0.1
-        viz_offset = [0.0, 0.0]
-        self.fem_sensor1.extract_markers(0)
-        init_2d = self.fem_sensor1.virtual_markers.to_numpy()
-        marker_2d = self.fem_sensor1.predict_markers.to_numpy()
-        self.draw_markers(init_2d, marker_2d, gui2)
-        self.draw_perspective(0)
-        gui1.circles(viz_scale * self.draw_pos3.to_numpy() + viz_offset, radius=2, color=0x039dfc)
-        gui1.circles(viz_scale * self.draw_pos2.to_numpy() + viz_offset, radius=2, color=0xe6c949)
-        # gui1.circles(viz_scale * self.draw_pos4.to_numpy() + viz_offset, radius=2, color=0xe6c949)
-        self.draw_triangles(self.fem_sensor1, gui3, 0, 0, 90, viz_scale, viz_offset)
-        gui1.show()
-        gui2.show()
-        gui3.show()
     
     def prepare_env(self):
         self.init_pos_control()
@@ -642,10 +626,6 @@ def main():
                 init_2d = contact_model.fem_sensor1.virtual_markers.to_numpy()
                 marker_2d = contact_model.fem_sensor1.predict_markers.to_numpy()
                 contact_model.draw_markers(init_2d, marker_2d, gui2)
-
-            ### the external force is not propogate to the last time step but the second last
-            # contact_model.draw_external_force(contact_model.fem_sensor1.sub_steps-2)
-            if not off_screen:
                 contact_model.draw_perspective(0)
                 gui1.circles(viz_scale * contact_model.draw_pos3.to_numpy() + viz_offset, radius=2, color=0x039dfc)
                 gui1.circles(viz_scale * contact_model.draw_pos2.to_numpy() + viz_offset, radius=2, color=0xe6c949)
@@ -716,20 +696,6 @@ def main():
                 contact_model.reset()
                 for ss in range(num_sub_steps - 1):
                     contact_model.update(ss)
-
-            if not off_screen:
-                contact_model.fem_sensor1.extract_markers(0)
-                init_2d = contact_model.fem_sensor1.virtual_markers.to_numpy()
-                marker_2d = contact_model.fem_sensor1.predict_markers.to_numpy()
-                contact_model.draw_markers(init_2d, marker_2d, gui2)
-                
-                contact_model.draw_perspective(0)
-                gui1.circles(viz_scale * contact_model.draw_pos3.to_numpy() + viz_offset, radius=2, color=0x039dfc)
-                gui1.circles(viz_scale * contact_model.draw_pos2.to_numpy() + viz_offset, radius=2, color=0xe6c949)           
-
-                gui1.show()
-                gui2.show()
-                # gui3.show()
 
         losses.append(loss_frame)
 
