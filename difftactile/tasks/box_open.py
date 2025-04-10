245ddbec783721091c1ebe137a4c1a083d7892df
Piotr Blaszyk <piopio555888@gmail.com>
2025-04-10 21:57:50 +0100

    debug off

--- difftactile/tasks/box_open.py
+++ difftactile/tasks/box_open.py
@@ -607,7 +607,7 @@ def transform_2d(point, angle, translate):
 
 
 def main():
-    ti.init(debug=True, offline_cache=False, arch=ti.gpu, device_memory_GB=9)
+    ti.init(debug=False, offline_cache=False, arch=ti.gpu, device_memory_GB=9)
 
     obj_name = "earpod-case.stl"
     num_sub_steps = 50
