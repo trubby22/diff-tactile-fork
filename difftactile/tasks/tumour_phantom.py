f0e87c2bc36a95038f9ebcc223da090e0f8a8b44
Piotr Blaszyk <piopio555888@gmail.com>
2025-04-11 22:28:23 +0100

    resolution

--- difftactile/tasks/tumour_phantom.py
+++ difftactile/tasks/tumour_phantom.py
@@ -506,7 +506,14 @@ class Contact:
 def main():
     ti.init(debug=False, offline_cache=False, arch=ti.gpu, device_memory_GB=9)
     if not off_screen:
-        window = ti.ui.Window("Tumour palpation", (int(1920 * 0.5), int(1080 * 0.5)))
+        screen_width = 1920
+        screen_height = 1080
+        grid_rows = 2
+        grid_cols = 2
+        window_width = screen_width // grid_cols
+        window_height = screen_height // grid_rows
+        window_res = (int(window_width * 0.5), int(window_height * 0.5))
+        window = ti.ui.Window("high-level camera", window_res)
         canvas = window.get_canvas()
         canvas.set_background_color((0, 0, 0))
         scene = ti.ui.Scene()
@@ -517,15 +524,15 @@ def main():
         camera.lookat(0, 0, 0)
         camera.fov(2.0)
         if enable_gui1:
-            gui1 = ti.GUI("Contact Viz")
+            gui1 = ti.GUI("low-level camera", res=list(window_res))
         else:
             gui1 = None
         if enable_gui2:
-            gui2 = ti.GUI("Force Map 1")
+            gui2 = ti.GUI("tactile readout 1", res=list(window_res))
         else:
             gui2 = None
         if enable_gui3:
-            gui3 = ti.GUI("Deformation Map 1")
+            gui3 = ti.GUI("tactile readout 2", res=list(window_res))
         else:
             gui3 = None
     phantom_name = "J03_2.obj"
